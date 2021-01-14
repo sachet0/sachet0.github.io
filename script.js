027f8e624228b1e0ee2c8326ea063ded9bcc75b2
Sachet <sachet0@users.noreply.github.com>
2021-01-14 14:37:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-document.body.onkeyup = function (e) {
-    if (e.code == "Space") {
-        document.body.classList.toggle("dark")
-    }
-}
-
 document.getElementById('name').onclick = function (e) {
     document.body.classList.toggle("light")
 }
